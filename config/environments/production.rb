ca31bc17a9624a545b07c9855adad8baade78519
piush-gupta <kumar.piush@tcs.com>
2017-06-05 16:21:26 +0530

    added mailer settings for prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,6 +77,17 @@ OdcInduction::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  # Mailer settings
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.smtp_settings = {
+  address:              'smtp.gmail.com',
+  port:                 587,
+  domain:               'gmail.com',
+  user_name:            'vaibhav.khl',
+  password:             '',
+  authentication:       'plain',
+  enable_starttls_auto: true  }
 end
 
 ROOT_URL = "https://still-eyrie-14623.herokuapp.com"
