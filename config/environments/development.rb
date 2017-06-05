fc16e39d081f4ce7218f96e7e2552bf96d4fe677
piush-gupta <kumar.piush@tcs.com>
2017-06-05 16:28:46 +0530

    added mailer settings for prod 2

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -17,7 +17,7 @@ OdcInduction::Application.configure do
   config.action_mailer.raise_delivery_errors = true
   
   # Mailer settings
-  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
   address:              'smtp.gmail.com',
   port:                 587,
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,7 +79,7 @@ OdcInduction::Application.configure do
   config.log_formatter = ::Logger::Formatter.new
 
   # Mailer settings
-  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
   address:              'smtp.gmail.com',
   port:                 587,
