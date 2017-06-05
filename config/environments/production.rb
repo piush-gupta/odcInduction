75f64f44db0bea6b87169fb8bf7a6efee4f255c8
piush-gupta <kumar.piush@tcs.com>
2017-06-05 16:32:36 +0530

    added mailer settings for prod 3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,8 +84,8 @@ OdcInduction::Application.configure do
   address:              'smtp.gmail.com',
   port:                 587,
   domain:               'gmail.com',
-  user_name:            'vaibhav.khl',
-  password:             '',
+  user_name:            'hello@andratio.co',
+  password:             'admin226688',
   authentication:       'plain',
   enable_starttls_auto: true  }
 end
